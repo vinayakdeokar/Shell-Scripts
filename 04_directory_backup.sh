1062a8920680fcc1e734d349d14eb7457f4d5aae
vinayakdeokar <vinayakdeokar121@gmail.com>
2025-10-17 00:40:42 +0530

    04_directory_backup.sh
    Removed redundant header comments and retained script functionality.

--- 04_directory_backup.sh
+++ 04_directory_backup.sh
@@ -8,15 +8,6 @@
 #######################################
 
 #!/bin/bash
-#######################################
-# Script Name :  backup.sh #
-# Version : 2.0 #
-# Date :  15/10/2025 #
-# Credit: Vinayak Deokar #
-# Purpose : Write a script that backs up a specified directory to a remote server using 'rsync' and SSH. #
-#######################################
-
-#!/bin/bash
 LOCAL_DIR="/home/vdeokar/ssh_document"
 REMOTE_USER="vinayakdeokar"
 REMOTE_HOST="192.168.1.7"
